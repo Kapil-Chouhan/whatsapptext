78cf0a48456c88ad4d27aafe6c1d14667be59a9a
rdsparky <57054408+rdsparky@users.noreply.github.com>
2020-10-13 20:33:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import pywhatkit as kit
 #sendwhatmsg() takes receiver phone number, text to be sent, also the hour and minute when it should be sent. 
 kit.sendwhatmsg("+91**********", "Hi, This message is sent via automated script written in python.", 20, 20)
 
+#Change +91********** to +91receiver's_mobile_number
+
 #Requirements for this code to woek:
 # 1. Google chrome browser
 # 2. Web.whatsapp logged in to Google chrome browser
